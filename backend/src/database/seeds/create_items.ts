6d42f715565720780ef98ae987dad2c27fd83f48
Carlos Eduardo <byedu1234@hotmail.com>
2020-06-05 22:19:45 -0300

    done web application

--- backend/src/database/seeds/create_items.ts
+++ backend/src/database/seeds/create_items.ts
@@ -4,7 +4,7 @@ export async function seed(knex: Knex) {
     await knex('items').insert([
         { title: 'Lâmpadas', image: 'lampadas.svg' },
         { title: 'Pilhas e Baterias', image: 'baterias.svg' },
-        { title: 'Papeis e Papelão', image: 'papeis-papelão.svg' },
+        { title: 'Papeis e Papelão', image: 'papeis-papelao.svg' },
         { title: 'Resíduoes Eletrônicos', image: 'eletronicos.svg' },
         { title: 'Resíduos Orgânicos', image: 'organicos.svg' },
         { title: 'Óleo de Cozinha', image: 'oleo.svg' },
